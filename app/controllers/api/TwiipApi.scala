965115df3acf73c6e15843aac71ad932239c5f35
Timo Hirt <timo.hirt@gmail.com>
2013-11-21 15:17:04 +0100

    created file

--- app/controllers/api/TwiipApi.scala
+++ app/controllers/api/TwiipApi.scala
@@ -6,8 +6,7 @@ import model.Twiip
 import play.api.libs.functional.syntax.functionalCanBuildApplicative
 import play.api.libs.functional.syntax.toFunctionalBuilderOps
 import play.api.libs.json.__
-import play.api.mvc.Action
-import play.api.mvc.Controller
+import play.api.mvc._
 import model.TwiipImplicits._
 
 object TwiipApi extends Controller {
