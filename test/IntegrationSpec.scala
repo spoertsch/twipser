ad6ce1de6d2b66954fbd716456364f5e08a4458c
Timo Hirt <timo.hirt@gmail.com>
2013-11-21 13:09:27 +0100

    added tests for message length and create message

--- test/IntegrationSpec.scala
+++ test/IntegrationSpec.scala
@@ -1,24 +1,45 @@
 import org.specs2.mutable._
 import org.specs2.runner._
 import org.junit.runner._
-
 import play.api.test._
 import play.api.test.Helpers._
+import org.specs2.execute.AsResult
 
 /**
  * add your integration spec here.
  * An integration test will fire up a whole play application in a real (or headless) browser
  */
 @RunWith(classOf[JUnitRunner])
-class IntegrationSpec extends Specification {
+class IntegrationSpec extends PlaySpecification {
 
-  "Application" should {
+  "Twipser" should {
+    
+    "display created twiips immediately" in new WithBrowser(webDriver = FIREFOX)  { 
+   	      val uuid = java.util.UUID.randomUUID.toString
 
-    "work from within a browser" in new WithBrowser(webDriver = FIREFOX) {
+	      browser.goTo("http://localhost:" + testServerPort + "/twiips")
+	      browser.$("#author").text("selenium")
+   	      browser.$("#message").text(uuid)
 
-      browser.goTo("http://localhost:" + port)
-
-      browser.pageSource must contain("Your new application is ready.")
+   	      browser.$("#create-twiip-form").submit()
+   	      browser.await()
+	      
+	      browser.pageSource must contain(uuid)
     }
+    
+    "complain about to long messages" in new WithBrowser(webDriver = FIREFOX) {
+
+	      browser.goTo("http://localhost:" + testServerPort + "/twiips")
+	      browser.$("#author").text("selenium")
+   	      browser.$("#message").text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse pulvinar porta tempus. In a nisi eget leo egestas aliquam nec.")
+
+   	      browser.$("#create-twiip-form").submit()
+   	      browser.await()
+	      
+	      browser.$(".error").getText() must contain("Maximum length is 120")
+    }
+    
+     
+    
   }
 }
