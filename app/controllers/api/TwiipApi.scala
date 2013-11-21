1608534463a89baca3b7277b05613656c8255da6
Timo Hirt <timo.hirt@gmail.com>
2013-11-21 14:39:03 +0100

    added skeleton of TwiipApi

---
+++ app/controllers/api/TwiipApi.scala
@@ -0,0 +1,15 @@
+package controllers.api
+
+import scala.concurrent.ExecutionContext.Implicits.global
+
+import model.Twiip
+import play.api.libs.functional.syntax.functionalCanBuildApplicative
+import play.api.libs.functional.syntax.toFunctionalBuilderOps
+import play.api.libs.json.__
+import play.api.mvc.Action
+import play.api.mvc.Controller
+import model.TwiipImplicits._
+
+object TwiipApi extends Controller {
+
+}
